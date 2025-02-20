00291a5d0868e61db21f792b2b9c164f23671f9e
shiloSaadon <shilosadon@gmail.com>
2025-02-20 21:24:06 +0200

    Fix cache issue

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1877205464"
+    serviceWorkerVersion: "630287930"
   }
 });
